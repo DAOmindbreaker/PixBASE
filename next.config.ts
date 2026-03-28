dccd79042f17300402e88d497b97e177e268838c
DAOmindbreaker <rio.pranata62@gmail.com>
2026-03-28 12:38:04 +0700

    Fix webpack resolve errors

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,16 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      "pino-pretty": false,
+      "lokijs": false,
+      "encoding": false,
+    };
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
 };
 
 export default nextConfig;
