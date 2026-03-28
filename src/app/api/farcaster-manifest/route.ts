6636aaf1255032a62a83f0debb1702bc6c2729a8
DAOmindbreaker <rio.pranata62@gmail.com>
2026-03-28 12:29:29 +0700

    Add Farcaster account association

--- src/app/api/farcaster-manifest/route.ts
+++ src/app/api/farcaster-manifest/route.ts
@@ -3,9 +3,11 @@ import { NextResponse } from "next/server";
 export async function GET() {
   return NextResponse.json({
     accountAssociation: {
-      header: "",
-      payload: "",
-      signature: "",
+      header:
+        "eyJmaWQiOjUzMjAzNCwidHlwZSI6ImF1dGgiLCJrZXkiOiIweDVGZkM2OTgwRkMwZDAwMzI1ODA1Q0U4OTU0RDM0MmE3N2ZGOWNmNDIifQ",
+      payload: "eyJkb21haW4iOiJiYXNlcGl4ZWxvbi52ZXJjZWwuYXBwIn0",
+      signature:
+        "FSdkm8AeM+zV2SiJaL/dyXqopAJhsKkCwSxFmPS7E0VHJo+c4+ja4nEiQ0zjTBNR4vCoIuFdQRyoCTl4/IhN7Rw=",
     },
     frame: {
       version: "1",
